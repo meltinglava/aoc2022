688f81fedef1148aa2487f83d3ad6b32a4fb0b8e
meltinglava <roi1996@gmail.com>
2022-12-01 19:22:31 +0100

    day1

---
+++ src/day1.rs
@@ -0,0 +1,17 @@
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input.split("\n\n").map(|n| n.lines().map(|n| n.parse::<usize>().unwrap()).sum()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &[usize]) -> usize {
+    *input.iter().max().unwrap()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &[usize]) -> usize {
+    let mut input = input.to_vec();
+    input.sort();
+    input.reverse();
+    input.iter().take(3).sum()
+}
