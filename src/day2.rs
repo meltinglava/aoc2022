6ed38b33950e9afc5ba83da68cd9448cd4c49d54
meltinglava <roi1996@gmail.com>
2022-12-02 17:21:45 +0100

    add templace

---
+++ src/day2.rs
@@ -0,0 +1,21 @@
+pub enum RPS {
+    Rock,
+    Paper,
+    Scisers,
+}
+
+
+#[aoc_generator(dayn)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+
+}
+
+#[aoc(day2, part1)]
+pub fn part1(input: &[usize]) -> usize {
+
+}
+
+// #[aoc(day2, part2)]
+// pub fn part2(input: &[usize]) -> usize {
+
+// }
